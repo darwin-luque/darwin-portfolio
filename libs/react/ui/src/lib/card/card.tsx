c059cb3a88cca4d04bf7214d2909c7c312f7a1ce
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-09 12:29:15 -0600

    feat(react/ui): add a card component

---
+++ libs/react/ui/src/lib/card/card.tsx
@@ -0,0 +1,73 @@
+import { motion, useMotionValue } from 'framer-motion';
+import { memo, ReactNode, useRef } from 'react';
+import {
+  useInvertedBorderRadius,
+  useScrollConstraints,
+  useWheelScroll,
+} from '@darwin-portfolio/react/hooks';
+import classes from './card.module.css';
+
+export const springs = {
+  open: { type: 'spring', stiffness: 200, damping: 30 },
+  close: { type: 'spring', stiffness: 300, damping: 35 },
+};
+
+const triggerDistance = 40;
+
+interface CardProps {
+  children: ReactNode;
+  useScrollEffect?: boolean;
+  onClick?: () => void;
+  onToggle?: (state: 'add' | 'remove') => void;
+}
+
+const Card = ({
+  children,
+  onClick,
+  onToggle,
+  useScrollEffect = false,
+}: CardProps) => {
+  const y = useMotionValue(0);
+  const zIndex = useMotionValue(0);
+  const inverted = useInvertedBorderRadius(20);
+  const cardRef = useRef<HTMLDivElement>(null);
+  const constraints = useScrollConstraints(cardRef, true);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const checkSwipe = () => {
+    if (!onToggle) return;
+    // use functionality to add or remove songs from library
+    if (y.get() > triggerDistance) {
+      onToggle('remove');
+    } else if (y.get() < -triggerDistance) {
+      onToggle('add');
+    }
+  };
+
+  if (useScrollEffect) {
+    useWheelScroll(containerRef, y, constraints, checkSwipe, true);
+  }
+
+  return (
+    <motion.div
+      whileHover={{ scale: 1.02 }}
+      className={classes['card']}
+      ref={containerRef}
+      onClick={onClick}
+    >
+      <div className={classes['card-content-container']}>
+        <motion.div
+          className={classes['card-content']}
+          style={{ ...inverted, zIndex, y }}
+          layout
+          ref={cardRef}
+          transition={springs.close}
+        >
+          {children}
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default memo(Card);
