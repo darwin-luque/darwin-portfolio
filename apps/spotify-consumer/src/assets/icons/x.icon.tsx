d21f731feb7008391a5c46cfaa427f95fe564c0e
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-09 22:57:09 -0600

    chore(spoitfy-consumer): animate search bar and continue styling the navbar

---
+++ apps/spotify-consumer/src/assets/icons/x.icon.tsx
@@ -0,0 +1,20 @@
+import { motion } from 'framer-motion';
+
+const XIcon = ({ size = 32, color = '#eee' }) => (
+  <motion.svg
+    viewBox="0 0 24 24"
+    role="presentation"
+    width={size}
+    height={size}
+  >
+    <motion.path
+      d="M17 7L7 17M7 7l5.03 5.03L17 17"
+      fill="transparent"
+      stroke-width="2"
+      stroke={color}
+      stroke-linecap="round"
+    />
+  </motion.svg>
+);
+
+export default XIcon;
--- apps/spotify-consumer/src/components/navbar/search-bar/search-bar.tsx
+++ apps/spotify-consumer/src/components/navbar/search-bar/search-bar.tsx
@@ -1,6 +1,46 @@
-import { motion } from 'framer-motion';
-import SearchIcon from '../../../assets/icons/search-icon';
+import { motion, useAnimation, Variants } from 'framer-motion';
+import { useEffect } from 'react';
+import SearchIcon from '../../../assets/icons/search.icon';
+import XIcon from '../../../assets/icons/x.icon';
 import classes from './search-bar.module.css';
+
+const fadeInOutVariants: Variants = {
+  initial: {
+    scale: 0,
+    opacity: 0,
+  },
+  center: {
+    scale: 1,
+    opacity: 1,
+  },
+  end: {
+    opacity: 0,
+    transition: {
+      delay: 0.5,
+    },
+  },
+};
+
+const inputVariants: Variants = {
+  initial: {
+    width: 0,
+    padding: 0,
+  },
+  center: {
+    width: '300px',
+    padding: '0 3.2em 0 1.5rem',
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: {
+    width: 0,
+    padding: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 interface SearchBarProps {
   showBar: boolean;
@@ -9,15 +49,31 @@ interface SearchBarProps {
 }
 
 const SearchBar = ({ showBar, onToggleBar, onInputChange }: SearchBarProps) => {
-  return showBar ? (
+  return (
     <div className={classes['container']}>
-      <SearchIcon />
-      <input onChange={(e) => onInputChange(e.target.value)} className={classes['input']} />
+      <motion.input
+        layout
+        variants={inputVariants}
+        data-showBar={showBar}
+        initial="initial"
+        animate={showBar ? 'center' : 'exit'}
+        onChange={(e) => onInputChange(e.target.value)}
+        className={classes['input']}
+        placeholder="Search"
+        autoFocus
+      />
+      <motion.button
+        variants={fadeInOutVariants}
+        initial="initial"
+        animate="center"
+        exit="end"
+        whileHover={{ cursor: 'pointer' }}
+        onClick={() => onToggleBar(!showBar)}
+        className={classes['button']}
+      >
+        {showBar ? <XIcon /> : <SearchIcon />}
+      </motion.button>
     </div>
-  ) : (
-    <motion.button onClick={() => onToggleBar(!showBar)} className={classes['single-search-icon']}>
-      <SearchIcon />
-    </motion.button>
   );
 };
 
