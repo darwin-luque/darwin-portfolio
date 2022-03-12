2e743ee1ade240f22fad0cb28327f29db0cf62c0
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-11 20:16:34 -0600

    refactor(react/ui-carousel): move items template to its own component

--- libs/react/ui/src/lib/carousel/carousel.tsx
+++ libs/react/ui/src/lib/carousel/carousel.tsx
@@ -1,33 +1,19 @@
 import { AnimatePresence, motion, Variants } from 'framer-motion';
 import { memo, useState } from 'react';
 import classes from './carousel.module.css';
-
-const variants: Variants = {
-  enter: (direction: number) => ({
-    x: direction > 0 ? 1000 : -1000,
-    opacity: 0,
-  }),
-  center: {
-    zIndex: 1,
-    x: 0,
-    opacity: 1,
-  },
-  exit: (direction: number) => ({
-    zIndex: 0,
-    x: direction < 0 ? 1000 : -1000,
-    opacity: 0,
-  }),
-};
+import Item from './item/item';
 
 interface CarouselProps<T> {
-  perPage: number;
   data: T[];
+  perPage: number;
+  itemMinWidth?: number;
   ElementTemplate: (props: T) => JSX.Element;
 }
 
 export const Carousel = <T extends { id: number | string }>({
-  perPage,
   data,
+  perPage,
+  itemMinWidth,
   ElementTemplate,
 }: CarouselProps<T>) => {
   const itemsPerPage = perPage > data.length ? data.length : perPage;
@@ -52,26 +38,9 @@ export const Carousel = <T extends { id: number | string }>({
       <AnimatePresence initial={false} custom={direction}>
         <motion.div className={classes['items']}>
           {[...data].splice(page, itemsPerPage).map((val) => (
-            <motion.div
-              custom={direction}
-              variants={variants}
-              initial="enter"
-              animate="center"
-              exit="exit"
-              transition={{
-                x: {
-                  type: 'spring',
-                  stiffness: 300,
-                  damping: 30,
-                  duration: 0.8,
-                },
-                opacity: { duration: 0.8 },
-              }}
-              key={val.id}
-              className={classes['item']}
-            >
+            <Item direction={direction} key={val.id} minWidth={itemMinWidth}>
               <ElementTemplate {...val} />
-            </motion.div>
+            </Item>
           ))}
         </motion.div>
       </AnimatePresence>
---
+++ libs/react/ui/src/lib/carousel/item/item.tsx
@@ -0,0 +1,51 @@
+import { motion, Variants } from 'framer-motion';
+import { ReactNode } from 'react';
+import classes from './item.module.css';
+
+const variants: Variants = {
+  enter: (direction: number) => ({
+    x: direction > 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+  center: {
+    zIndex: 1,
+    x: 0,
+    opacity: 1,
+  },
+  exit: (direction: number) => ({
+    zIndex: 0,
+    x: direction < 0 ? 1000 : -1000,
+    opacity: 0,
+  }),
+};
+
+interface ItemProps {
+  direction: 1 | -1;
+  minWidth?: number;
+  children: ReactNode;
+}
+
+const Item = ({ direction, children, minWidth }: ItemProps) => (
+  <motion.div
+    style={{ minWidth }}
+    custom={direction}
+    variants={variants}
+    initial="enter"
+    animate="center"
+    exit="exit"
+    transition={{
+      x: {
+        type: 'spring',
+        stiffness: 300,
+        damping: 30,
+        duration: 0.8,
+      },
+      opacity: { duration: 0.8 },
+    }}
+    className={classes['item']}
+  >
+    {children}
+  </motion.div>
+);
+
+export default Item;
