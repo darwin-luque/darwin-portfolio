fde1f91ea8352f01cda48cfa57a31e6a29e67ac3
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-11 21:31:49 -0600

    feat(sp[otify-consumer): show the tracks add to the library on its screen

--- apps/spotify-consumer/src/screens/library/library.screen.tsx
+++ apps/spotify-consumer/src/screens/library/library.screen.tsx
@@ -1,7 +1,17 @@
+import TrackCard from '../../components/track-card/track-card';
+import { useAppSelector } from '../../hooks/redux-hooks';
+import classes from './library.module.css';
+
 const Library = () => {
+  const tracks = useAppSelector((state) => state.library.tracks);
+
   return (
-    <div>
-      <h1>Library</h1>
+    <div className={classes['tracks']}>
+      {tracks.map((track) => (
+        <div key={track.id} className={classes['track']}>
+          <TrackCard useScrollEffect={false} {...track} />
+        </div>
+      ))}
     </div>
   );
 };
