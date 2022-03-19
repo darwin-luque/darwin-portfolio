9371d2d4291540b50ed45f83eb2f5bb2091aa011
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-19 17:07:22 -0600

    chore(spotify-consumer): add testing-library setup files

--- apps/spotify-consumer/jest.config.js
+++ apps/spotify-consumer/jest.config.js
@@ -6,11 +6,13 @@ module.exports = {
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleNameMapper: {
-    'firebase/app': `${__dirname}/src/__mocks__/firebase/app.js`,
-    'firebase/auth': `${__dirname}/src/__mocks__/firebase/auth.js`,
-    'firebase/database': `${__dirname}/src/__mocks__/firebase/database.js`,
-    'firebase/firestore': `${__dirname}/src/__mocks__/firebase/firestore.js`,
+    'firebase/app': '<rootDir>/src/__mocks__/firebase/app.js',
+    'firebase/auth': '<rootDir>/src/__mocks__/firebase/auth.js',
+    'firebase/database': '<rootDir>/src/__mocks__/firebase/database.js',
+    'firebase/firestore': '<rootDir>/src/__mocks__/firebase/firestore.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/spotify-consumer',
+  setupFiles: ['<rootDir>/src/__tests__/setup.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupAfterEnv.ts'],
 };
