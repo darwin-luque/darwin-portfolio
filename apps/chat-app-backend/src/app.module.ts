0c9d8ae73d13e297a1d804ff28c0839de8408c4d
Darwin Luque <darwin.luque.98@gmail.com>
2022-03-30 21:49:51 -0600

    chore(chat-app-backend): configure orm

--- apps/chat-app-backend/src/app.module.ts
+++ apps/chat-app-backend/src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 
+const configs = {
+  synchronize: process.env.NODE_ENV === 'development',
+  type: 'postgres',
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DB_NAME,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'migrations',
+  },
+};
+
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, TypeOrmModule.forRoot(configs)],
   controllers: [],
   providers: [],
 })
